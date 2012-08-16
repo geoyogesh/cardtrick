1c633e65f3496594beb94a44b9bcaaa4fa75763f
Yogesh Dhanapal <geoyogesh@gmail.com>
2012-08-16 13:39:30 +0530

    initial program

---
+++ CardTrick/CardTrickWinForm/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CardTrickWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnpool3 = new System.Windows.Forms.Button();
+            this.btnpool2 = new System.Windows.Forms.Button();
+            this.lbxpool3 = new System.Windows.Forms.ListBox();
+            this.lbxpool2 = new System.Windows.Forms.ListBox();
+            this.lbxpool1 = new System.Windows.Forms.ListBox();
+            this.btnpool1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnpool3
+            // 
+            this.btnpool3.Location = new System.Drawing.Point(345, 77);
+            this.btnpool3.Name = "btnpool3";
+            this.btnpool3.Size = new System.Drawing.Size(75, 23);
+            this.btnpool3.TabIndex = 11;
+            this.btnpool3.Text = "pool3";
+            this.btnpool3.UseVisualStyleBackColor = true;
+            this.btnpool3.Click += new System.EventHandler(this.btnpool3_Click);
+            // 
+            // btnpool2
+            // 
+            this.btnpool2.Location = new System.Drawing.Point(204, 77);
+            this.btnpool2.Name = "btnpool2";
+            this.btnpool2.Size = new System.Drawing.Size(75, 23);
+            this.btnpool2.TabIndex = 10;
+            this.btnpool2.Text = "Pool2";
+            this.btnpool2.UseVisualStyleBackColor = true;
+            this.btnpool2.Click += new System.EventHandler(this.btnpool2_Click);
+            // 
+            // lbxpool3
+            // 
+            this.lbxpool3.FormattingEnabled = true;
+            this.lbxpool3.Location = new System.Drawing.Point(329, 120);
+            this.lbxpool3.Name = "lbxpool3";
+            this.lbxpool3.Size = new System.Drawing.Size(120, 95);
+            this.lbxpool3.TabIndex = 9;
+            // 
+            // lbxpool2
+            // 
+            this.lbxpool2.FormattingEnabled = true;
+            this.lbxpool2.Location = new System.Drawing.Point(179, 120);
+            this.lbxpool2.Name = "lbxpool2";
+            this.lbxpool2.Size = new System.Drawing.Size(120, 95);
+            this.lbxpool2.TabIndex = 8;
+            // 
+            // lbxpool1
+            // 
+            this.lbxpool1.FormattingEnabled = true;
+            this.lbxpool1.Location = new System.Drawing.Point(32, 120);
+            this.lbxpool1.Name = "lbxpool1";
+            this.lbxpool1.Size = new System.Drawing.Size(120, 95);
+            this.lbxpool1.TabIndex = 7;
+            // 
+            // btnpool1
+            // 
+            this.btnpool1.Location = new System.Drawing.Point(53, 77);
+            this.btnpool1.Name = "btnpool1";
+            this.btnpool1.Size = new System.Drawing.Size(75, 23);
+            this.btnpool1.TabIndex = 6;
+            this.btnpool1.Text = "Pool1";
+            this.btnpool1.UseVisualStyleBackColor = true;
+            this.btnpool1.Click += new System.EventHandler(this.btnpool1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 372);
+            this.Controls.Add(this.btnpool3);
+            this.Controls.Add(this.btnpool2);
+            this.Controls.Add(this.lbxpool3);
+            this.Controls.Add(this.lbxpool2);
+            this.Controls.Add(this.lbxpool1);
+            this.Controls.Add(this.btnpool1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnpool3;
+        private System.Windows.Forms.Button btnpool2;
+        private System.Windows.Forms.ListBox lbxpool3;
+        private System.Windows.Forms.ListBox lbxpool2;
+        private System.Windows.Forms.ListBox lbxpool1;
+        private System.Windows.Forms.Button btnpool1;
+    }
+}
+
---
+++ CardTrick/CardTrickWinForm/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace CardTrickWinForm
+{
+    public partial class Form1 : Form
+    {
+        List<int> deck = new List<int>();
+        List<int> pool1 = new List<int>();
+        List<int> pool2 = new List<int>();
+        List<int> pool3 = new List<int>();
+        int turn = 1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showresult()
+        {
+            MessageBox.Show(deck[Convert.ToInt32(ConfigurationManager.AppSettings["pointer"].ToString())].ToString());
+            Form1_Load(null, null);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            turn = 1;
+            Initializedeck();
+            dissemimate();
+            print();
+        }
+
+        private void print()
+        {
+            lbxpool1.DataSource = pool1;
+            lbxpool2.DataSource = pool2;
+            lbxpool3.DataSource = pool3;
+        }
+
+        private void dissemimate()
+        {
+            pool1 = new List<int>();
+            pool2 = new List<int>();
+            pool3 = new List<int>();
+            for (int i = 0; i < deck.Count; i++)
+            {
+                pool1.Add(deck[i]);
+                pool2.Add(deck[i + 1]);
+                pool3.Add(deck[i + 2]);
+                i += 2;
+            }
+        }
+
+        private void Initializedeck()
+        {
+            int count = Convert.ToInt32(ConfigurationManager.AppSettings["total"].ToString());
+            count++;
+
+            deck = new List<int>();
+            for (int i = 1; i < count; i++)
+            {
+                deck.Add(i);
+            }
+        }
+
+        private void btnpool1_Click(object sender, EventArgs e)
+        {
+            deck = new List<int>();
+            deck.AddRange(pool2);
+            deck.AddRange(pool1);
+            deck.AddRange(pool3);
+
+            iteration();
+        }
+
+        private void iteration()
+        {
+            deck.Reverse();
+            if (turn != 3)
+            {
+                dissemimate();
+                print();
+                turn++;
+            }
+            else
+            {
+                showresult();
+            }
+        }
+
+        private void btnpool2_Click(object sender, EventArgs e)
+        {
+            deck = new List<int>();
+            deck.AddRange(pool1);
+            deck.AddRange(pool2);
+            deck.AddRange(pool3);
+
+            iteration();
+        }
+
+        private void btnpool3_Click(object sender, EventArgs e)
+        {
+            deck = new List<int>();
+            deck.AddRange(pool1);
+            deck.AddRange(pool3);
+            deck.AddRange(pool2);
+
+            iteration();
+        }
+    }
+}
